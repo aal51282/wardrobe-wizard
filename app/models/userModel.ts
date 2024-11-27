8c9cdea5628f2e55d993f68b53397887e995fb47
Angel <aal51282@uga.edu>
2024-11-27 12:49:07 -0500

    Added a check to make sure that the username is removed.

--- app/models/userModel.ts
+++ app/models/userModel.ts
@@ -32,6 +32,24 @@ const userSchema = new Schema<IUser>({
     }
 });
 
+// Drop the username index if it exists when the model is compiled
+userSchema.pre('save', async function(next) {
+    try {
+        // Check if the collection exists and has the username index
+        const collections = await mongoose.connection.db.listCollections({ name: 'users' }).toArray();
+        if (collections.length > 0) {
+            const indexes = await mongoose.connection.db.collection('users').indexes();
+            const hasUsernameIndex = indexes.some(index => index.name === 'username_1');
+            if (hasUsernameIndex) {
+                await mongoose.connection.db.collection('users').dropIndex('username_1');
+            }
+        }
+        next();
+    } catch (error) {
+        next(error as Error);
+    }
+});
+
 const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
 
 export default User;
